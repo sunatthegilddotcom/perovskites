028df6bc3b92a24912849992c311734059c6826f
Anne Farley <afarley9@uw.edu>
2021-03-08 14:23:28 -0800

    moved folders around

--- codebase/__init__.py
+++
@@ -1,4 +0,0 @@
-from .version import __version__
-from .codebase import predict
-
-__all__ = [__version__, predict]
--- perovskites/__init__.py
+++
@@ -1,4 +0,0 @@
-from .version import __version__
-from utils.codebase import predict
-
-__all__ = [__version__, predict]
