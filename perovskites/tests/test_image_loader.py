c217ed0ac024fd912a97f30647478c335581c705
Anne Farley <afarley9@uw.edu>
2021-03-13 14:11:40 -0800

    framework for tests

---
+++ perovskites/tests/test_image_loader.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import unittest
+
+import numpy as np
+from perovskites.utils import image_loader
+
+
+class test_image_loader(unittest.TestCase):
+
+    def test_sample(self):
+        """
+        Tests sample() function within PLDataLoader
+        class.
+        """
+        return None
+
+    def test_train_test(self):
+        """
+        Tests train_test_split() function within
+        PLDataLoader class.
+        """
+        return None
---
+++ perovskites/tests/test_image_processing.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import unittest
+
+from perovskites.utils import image_processing
+
+
+class test_image_processing(unittest.TestCase):
+
+    def test_read_image(self):
+        """
+        Tests read_image() function.
+        """
+        return None
+
+    def test_mean_over_depth(self):
+        """
+        Tests mean_over_depth() function.
+        """
+        return None
+
+    def test_normalize(self):
+        """
+        Tests normalize() function.
+        """
+        return None
+
+    def test_crop_image(self):
+        """
+        Tests crop_image() function.
+        """
+        return None
+
+    def test_get_channel(self):
+        """
+        Tests get_channel() function.
+        """
+        return None
+
+    def test_resize_base_on_FOV(self):
+        """
+        Tests resize_based_on_FOV() function.
+        """
+        return None
+
+    def test_img_as_feed(self):
+        """
+        Tests img_as_feed() function.
+        """
+        return None
