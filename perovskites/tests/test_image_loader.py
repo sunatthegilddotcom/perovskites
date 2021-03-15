63eb351e96230a6a68f64cbc7b4cc5b569fd73fd
Anne Farley <afarley9@uw.edu>
2021-03-15 11:20:40 -0700

    flake8 test file

--- perovskites/tests/test_image_loader.py
+++ perovskites/tests/test_image_loader.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import unittest
 
 from perovskites.utils import image_loader as loader
@@ -8,24 +6,24 @@ from perovskites.utils import image_loader as loader
 class test_image_loader(unittest.TestCase):
 
     def __init__(self):
-			"""
-			Initializes class
-			"""
+        """
+        Initializes class
+        """
         self.dataset = loader.PLDataLoader()
 
     def test_predict(self):
         """
         Tests if the PLDataLoader() class correctly samples PL image data
         """
-        sample_path = "drive/Shareddrives/Perovskites_DIRECT/"+ (
-					"sample_image_Ld80_data.pickle")
+        sample_path = "drive/Shareddrives/Perovskites_DIRECT/" + (
+          "sample_image_Ld80_data.pickle")
         dataset = self.dataset
         sample_data = dataset.sample(frac=0.008)
         sample_data2 = loader._get_data_from_pickle(sample_path)
         self.assertIsInstance(sample_data, tuple)
         self.assertTrue(sample_data[0].shape[0] == 9)
         for i in range(len(sample_data)):
-          self.assertTrue(sample_data[i].any() == sample_data2[i].any())
+            self.assertTrue(sample_data[i].any() == sample_data2[i].any())
 
     def test_train_test_split(self):
         """
